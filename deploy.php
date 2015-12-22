6912b542670687e6147368da5d9e6eafa577c0c2
Kristian Kaa <kristian.kaa@obrigado.dk>
2015-12-22 15:45:22 +0100

    Updated deploy script

--- deploy.php
+++ deploy.php
@@ -9,7 +9,7 @@ server('prod', '46.101.170.221', 9999)
 ->user('kka')
 ->identityFile()
 ->stage('production')
-->env('deploy_path', '/var/www'); // Define the base path to deploy your project to.
+->env('deploy_path', '/usr/share/nginx/kristiankaadk'); // Define the base path to deploy your project to.
 
 // Specify the repository from which to download your project's code.
 // The server needs to have git installed for this to work.
@@ -17,19 +17,28 @@ server('prod', '46.101.170.221', 9999)
 // your project from this repository.
 set('repository', 'git@github.com:kaa4ever/kristiankaadk.git');
 
+// Set the symlinks.
+task('deploy:symlink', function () {
+  cd('{{release_path}}/site/sites/default');
+  run('ln -s /usr/share/nginx/kristiankaadk/shared/sites/default/settings.php');
+  run('ln -s /usr/share/nginx/kristiankaadk/shared/sites/default/services.yml');
+  run('ln -s /usr/share/nginx/kristiankaadk/shared/sites/default/files');
+  run('rm /usr/share/nginx/kristiankaadk/current');
+  run('ln -s {{release_path}} /usr/share/nginx/kristiankaadk/current');
+})->desc('Setting symlinks');
+
 task('build', function() {
-  write('Starting to build site');
-  cd('/var/www/html');
+  cd('{{release_path}}/site');
   run('drush make site.make -y');
 });
 
 task('drush', function() {
   write('Updating database');
-  cd('/var/www/html');
+  cd('/usr/share/nginx/kristiankaadk/current/site');
   run('drush updb -y');
   write('Rebuilding cache');
   run('drush cr');
 });
 
-after('deploy', 'build');
+after('deploy:shared', 'build');
 after('deploy', 'drush');
